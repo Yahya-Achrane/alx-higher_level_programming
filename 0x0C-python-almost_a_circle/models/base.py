cf17cdc3305283ca6baeb23d9d9372b5b4166252
Yahya Achrane <yahya.achrane12@gmail.com>
2023-09-18 13:48:18 -0700

    file to instance

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -8,6 +8,7 @@ Base class for managing id attribute in future classes.
 import csv
 import json
 
+
 class Base:
     """
 
@@ -26,7 +27,7 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
-    
+
     @staticmethod
     def to_json_string(list_dictionaries):
         """
@@ -87,3 +88,16 @@ class Base:
             dummy = cls(1)
         dummy.update(**dictionary)
         return dummy
+
+    @classmethod
+    def load_from_file(cls):
+        """
+        return a list of instances
+        """
+        filename = cls.__name__ + ".json"
+        try:
+            with open(filename, "r") as file:
+                list_dicts = cls.from_json_string(file.read())
+            return [cls.create(**dictionary) for dictionary in list_dicts]
+        except FileNotFoundError:
+            return []
